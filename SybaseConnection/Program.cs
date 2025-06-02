d37806a8780f22527afc23b07d58c73cdf725740
Angel <angelaragonf@gmail.com>
2025-06-02 17:01:59 -0600

    implementación de ODBC de Advantage + pruebas con diferentes strings de conexión

--- SybaseConnection/Program.cs
+++ SybaseConnection/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ADP = Advantage.Data.Provider;
-using ODBC = System.Data.OleDb;
+using ODBC = System.Data.Odbc;
 
 namespace SybaseConnection
 {
@@ -17,7 +18,7 @@ namespace SybaseConnection
             //C:\\Program Files (x86)\\Advantage 12.0\\Help\\ADS_DATA\\
             //objeto de conexión
 
-            string connectionString = "data source=C:\\Users\\HP\\Documents\\Angel\\SDG\\Sybase\\Data;" +
+            /*string connectionString = "data source=C:\\Users\\HP\\Documents\\Angel\\SDG\\Sybase\\Data;" +
                 "ServerType=local; TableType=ADT; CharType=OEM; LockMode=proprietary;" +
                 "SecurityMode=ignorerights; ShowDeleted=false; TrimTrailingSpaces=true;"; // CharType=ANSI CommType=default
             //ADP.AdsConnection conn = new ADP.AdsConnection(connectionString);
@@ -25,20 +26,28 @@ namespace SybaseConnection
             conn.ConnectionString = connectionString;
             ADP.AdsCommand cmd;
             ADP.AdsDataReader reader;
-            int iField;
+            int iField;*/
 
-            /*string connectionString = "Provider=Advantage OLE DB Provider;" +
-                "Data Source=C:\\Users\\HP\\Documents\\Angel\\SDG\\Sybase\\Data;" +
-                "Advantage Server Type=ADS_LOCAL_SERVER;";
+            string connectionString = @"Driver={Advantage StreamlineSQL ODBC};
+                          ServerTypes=local;
+                          DataDirectory=C:\\Users\\HP\\Documents\\Angel\\SDG\\Sybase\\Data;  
+                          CharSet=ANSI;
+                          TrimTrailingSpaces=True;
+                          Locking=FALSE;"; //CollationSequence=GENERAL_VFP_CI_AS_1252; CharSet=ANSI;
 
-            ODBC.OleDbConnection conn = new ODBC.OleDbConnection(connectionString);*/
+            ODBC.OdbcConnection conn = new ODBC.OdbcConnection(connectionString);
 
             try
             {
-                /*conn.Open();
-                ODBC.OleDbCommand cmd = new ODBC.OleDbCommand("SELECT * FROM Card", conn);
-                ODBC.OleDbDataReader reader = cmd.ExecuteReader();
+                conn.Open();
+                /*Console.WriteLine("Conexión exitosa");
+                OdbcCommand cmdCollation = new OdbcCommand("SET COLLATION 'GENERAL_VFP_CI_AS_1252'", conn);
+                cmdCollation.ExecuteNonQuery();*/
 
+
+                ODBC.OdbcCommand cmd = new ODBC.OdbcCommand("SELECT * FROM Card ORDER BY PkData", conn);
+                ODBC.OdbcDataReader reader = cmd.ExecuteReader();
+                Console.WriteLine("Select exitoso.");
                 while (reader.Read())
                 {
                     for(int i=0; i<reader.FieldCount; i++)
@@ -47,9 +56,9 @@ namespace SybaseConnection
                     }
                     Console.WriteLine();
                 }
-                reader.Close();*/
+                reader.Close();
 
-                conn.Open();
+                /*conn.Open();
                 cmd = conn.CreateCommand();
                 cmd.CommandText = "SELECT * FROM Card WHERE ROWID() > 0"; //select account from Area        select * from Area          select * from employee
                 reader = cmd.ExecuteReader();
@@ -64,8 +73,8 @@ namespace SybaseConnection
                     Console.WriteLine();
 
                 }
-                conn.Close();
-            }catch(ADP.AdsException ex /*Exception ex*/)
+                conn.Close();*/
+            }catch(/*ADP.AdsException ex*/ Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
